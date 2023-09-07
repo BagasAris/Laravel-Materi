31d6ee16793bb42ace1e745ed0dd63af9473a5e8
Bagas Aris Prasetyo <arisbagas278@gmail.com>
2023-09-07 11:49:48 +0700

    Materi Migrations

--- laravel-materi/database/migrations/2023_09_07_043812_create_films_table.php
+++ laravel-materi/database/migrations/2023_09_07_043812_create_films_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('films', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('judul', 45);
+            $table->text('ringkasan');
+            $table->integer('tahun');
+            $table->string('poster');
+            $table->foreignId('genre_id')->constrained('genres');
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('films');
     }
 };
--- laravel-materi/database/migrations/2023_09_07_044433_create_perans_table.php
+++ laravel-materi/database/migrations/2023_09_07_044433_create_perans_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('perans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nama', 45);
+            $table->foreignId('film_id')->constrained('films');
+            $table->foreignId('cast_id')->constrained('casts');
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('perans');
     }
 };
